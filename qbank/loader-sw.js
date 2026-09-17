f32b260b9512c7fba83ce0ff0d31c45fac48e482
curas1215 <dcurasy@gmail.com>
2026-09-17 14:00:23 +0800

    Update qbank loader service worker cache version

--- qbank/loader-sw.js
+++ qbank/loader-sw.js
@@ -1,8 +1,8 @@
-const SHELL='qbank-loader-v1.0.0';
+const SHELL='qbank-loader-v1.0.1';
 const RUNTIME='qbank-runtime-v1.0.0';
 const SHELL_FILES=['./','./index.html','./manifest.webmanifest'];
 self.addEventListener('install',event=>event.waitUntil(caches.open(SHELL).then(c=>c.addAll(SHELL_FILES)).then(()=>self.skipWaiting())));
-self.addEventListener('activate',event=>event.waitUntil(self.clients.claim()));
+self.addEventListener('activate',event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k.startsWith('qbank-loader-')&&k!==SHELL).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
 self.addEventListener('fetch',event=>{
   if(event.request.method!=='GET') return;
   const url=new URL(event.request.url);
